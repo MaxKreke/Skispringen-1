95717e03ee578e1d90cdfe5110aa695759e6680b
MaxKreke <max.kreke@rub.de>
2024-08-30 20:29:08 +0200

    added goblin cube sounds

--- Assets/Scripts/GoblinCube.cs
+++ Assets/Scripts/GoblinCube.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class GoblinCube : MonoBehaviour
 {
+    public AudioClip life;
+    public AudioClip death;
+
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(life, transform.position, 1f);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Destructible")
         {
             Destroy(collision.gameObject);
+            AudioSource.PlayClipAtPoint(death, transform.position, .5f);
             Destroy(this.gameObject);
         }
         if (collision.gameObject.tag == "Feuer")
         {
+            AudioSource.PlayClipAtPoint(death, transform.position, .5f);
             Destroy(this.gameObject);
         }
     }
+
 }

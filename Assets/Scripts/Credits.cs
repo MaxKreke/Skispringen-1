133d4c0211dbf1ccf2f316920cc38799d07992ed
MaxKreke <max.kreke@rub.de>
2024-09-02 03:49:29 +0200

    added scene loading

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Next.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer vp;
+
+    public string[] scenes;
+    public UnityEngine.Video.VideoClip[] clips;
+
+    private void Start()
+    {
+        vp.clip = clips[LevelTracker.level];
+        vp.loopPointReached += loadNext;
+    }
+
+    void loadNext(UnityEngine.Video.VideoPlayer vp)
+    {
+        Skip();
+    }
+
+    public void Update()
+    {
+        vp.Play();
+        if(Input.GetKey(KeyCode.Semicolon))Skip();
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene(scenes[LevelTracker.level]);
+    }
+}
